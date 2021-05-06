69c90a8cfc3fc1cd05d63e65ce7939bae4082fe0
Supernovanan <58990107+Supernovanan@users.noreply.github.com>
2021-05-06 14:36:18 -0500

    Archivo base

--- snake.py
+++ snake.py
@@ -17,15 +17,16 @@ food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+#Cambia los valores del vector
 def change(x, y):
     "Change snake direction."
     aim.x = x
     aim.y = y
-
+#determina si se queda dentro del límite
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
-
+#funcion principal de movimiento
 def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
@@ -54,13 +55,21 @@ def move():
     update()
     ontimer(move, 100)
 
+def movesquare():
+    food.x = randrange(-15, 15) * 10
+    food.y = randrange(-15, 15) * 10
+    ontimer(movesquare, 4500)
+
+#anima snake
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
+#
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
 move()
+movesquare()
 done()
